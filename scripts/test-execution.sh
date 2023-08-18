f1be54a5013dad581a70fc2f90cc21c69d6cc370
ATH\vmenta <Varvara.MENTA@intrasoft-intl.com>
2023-08-18 11:09:45 +0300

    Add execution sh

--- scripts/test-execution.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mvn clean verify -Dappium.hub=http://127.0.0.1:4723/wd/hub -Dwebdriver.driver=appium -Dappium.platformName=Android -Dappium.deviceName="Pixel 6 API 32" -Dcucumber.options="--tags '@ANDROID'" "$@"
-mvn serenity:aggregate
