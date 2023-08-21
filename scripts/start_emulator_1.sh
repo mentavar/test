4feb1e69d5e99685362d1387ee8e62395a7a4100
ftheofil <138104891+ftheofil@users.noreply.github.com>
2023-08-21 13:15:39 +0300

    Update start_emulator_1.sh

--- scripts/start_emulator_1.sh
+++ scripts/start_emulator_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Start the Android Emulator
-emulator -avd test -no-window -no-audio &
+emulator -avd Pixel 6 API 33 -no-window -no-audio &
 echo "Waiting for the emulator to start..."
 android-wait-for-emulator
