c5139084e1d211a214528ca1b26858658fc6af20
ftheofil <138104891+ftheofil@users.noreply.github.com>
2023-08-21 17:03:38 +0300

    Update emulator-up.sh

--- scripts/emulator-up.sh
+++ scripts/emulator-up.sh
@@ -7,17 +7,17 @@ set -e #stop immediately if any error happens
 avd_name=$1
 
 if [[ -z "$avd_name" ]]; then
-  avd_name="Pixel 6 API 33"
+  avd_name="test"
 fi
 
 #check if emulator work well
 emulator -version
 
 # create virtual device, default using Android 9 Pie image (API Level 28)
-echo no | avdmanager create avd -n Pixel 6 API 33 -k "system-images;android-33;google_apis;x86_64"
+echo no | avdmanager create avd -n test -k "system-images;android-29;google_apis;x86"
 
 # start the emulator
-emulator -avd Pixel 6 API 33 -no-audio -no-window -no-accel &
+emulator -avd Pixel test -no-audio -no-window -no-accel &
 
 # show connected virtual device
 adb devices
