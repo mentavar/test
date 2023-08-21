ba68be6f55abb6b0821e60d2d6de471b7c3f7068
ftheofil <138104891+ftheofil@users.noreply.github.com>
2023-08-21 13:13:49 +0300

    Update test-execution.sh

--- test-execution.sh
+++ test-execution.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mvn clean verify -Dappium.hub=http://127.0.0.1:4723/wd/hub -Dwebdriver.driver=appium -Dappium.platformName=Android -Dappium.deviceName="test" -Dcucumber.options="--tags '@ANDROID'" "$@"
+mvn clean verify -Dappium.hub=http://127.0.0.1:4723/wd/hub -Dwebdriver.driver=appium -Dappium.platformName=Android -Dappium.deviceName="Pixel 6 API 33" -Dcucumber.options="--tags '@ANDROID'" "$@"
 mvn serenity:aggregate
