9c7ca77757a06dbc3a61e2b27c47f866f25fbc53
ftheofil <138104891+ftheofil@users.noreply.github.com>
2023-08-21 11:57:09 +0300

    Update emulator-up.sh

--- scripts/emulator-up.sh
+++ scripts/emulator-up.sh
@@ -7,17 +7,17 @@ set -e #stop immediately if any error happens
 avd_name=$1
 
 if [[ -z "$avd_name" ]]; then
-  avd_name="test API"
+  avd_name="test"
 fi
 
 #check if emulator work well
 emulator -version
 
 # create virtual device, default using Android 9 Pie image (API Level 28)
-echo no | avdmanager create avd -n test API -k "system-images;android-29;google_apis;x86"
+echo no | avdmanager create avd -n test -k "system-images;android-29;google_apis;x86"
 
 # start the emulator
-emulator -avd test API -no-audio -no-window -no-accel &
+emulator -avd test -no-audio -no-window -no-accel &
 
 # show connected virtual device
 adb devices
