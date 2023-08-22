eb99b600ba379b3686871c79e333e623a5a5c1ce
ftheofil <138104891+ftheofil@users.noreply.github.com>
2023-08-22 12:38:19 +0300

    Update RunAppiumServer.sh

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -2,6 +2,7 @@
 set -ex
 npm install -g appium
 appium -v
-appium --address 127.0.0.1 --port 4723 
 appium driver install uiautomator2
+appium driver update uiautomator2
+appium --address 127.0.0.1 --port 4723 
 appium &>/dev/null &
