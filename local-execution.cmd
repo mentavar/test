f58216502899afad42b30292dec93b0534e26d5b
ftheofil <138104891+ftheofil@users.noreply.github.com>
2023-08-22 14:37:42 +0300

    Update local-execution.cmd

--- local-execution.cmd
+++ local-execution.cmd
@@ -1,2 +1,2 @@
-call mvn clean verify -Dappium.hub=http://127.0.0.1:4723/wd/hub -Dwebdriver.driver=appium -Dappium.platformName=Android -Dappium.deviceName="Pixel_6_API_33" -Dcucumber.options="--tags '@ANDROID'" %*
+call mvn clean verify -Dappium.hub=http://127.0.0.1:4723/wd/hub -Dwebdriver.driver=appium -Dappium.platformName=Android -Dappium.deviceName="Pixel_6_API_33_1" -Dcucumber.options="--tags '@ANDROID'" %*
 call mvn serenity:aggregate
