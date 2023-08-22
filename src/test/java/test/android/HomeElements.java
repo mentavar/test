a8c7d8f1254469270b278199be7a783af5115dd7
ftheofil <138104891+ftheofil@users.noreply.github.com>
2023-08-22 15:30:45 +0300

    Update HomeElements.java

--- src/test/java/test/android/HomeElements.java
+++ src/test/java/test/android/HomeElements.java
@@ -5,4 +5,5 @@ import org.openqa.selenium.By;
 public class HomeElements {
 
     public static final By homePageLogo = By.id("com.google.android.youtube:id/youtube_logo");
+    public static final By clickButton = By.id("com.android.permissioncontroller:id/permission_allow_button");
 }
