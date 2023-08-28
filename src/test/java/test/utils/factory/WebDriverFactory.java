d150affb1ccc5209dc9af6bfb62cc501992e835e
mentavar <133885795+mentavar@users.noreply.github.com>
2023-08-28 09:16:28 +0300

    increase wait to 1 hour

--- src/test/java/test/utils/factory/WebDriverFactory.java
+++ src/test/java/test/utils/factory/WebDriverFactory.java
@@ -58,7 +58,7 @@ public class WebDriverFactory {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        wait = new WebDriverWait(driver1, 600);
+        wait = new WebDriverWait(driver1, 3600);
     }
 
     public AndroidDriver<MobileElement> getDriver() {
